34c1ef860b709b7cfa6516b53cf5c89e4a3a4ea1
Isabel <izheng60@cslab3-2>
2025-10-30 11:47:24 -0400

    isabel

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,12 @@ app = Flask(__name__)    #create Flask object
 @app.route("/")
 def main_page():
     return "this is the app!!"
-
+@app.route("/register", methods = ['GET', 'POST'])
+def register(): 
+	message = ' '
+	if request.method == 'POST' and 'name' in request.form and 'password'in request.form: 
+		username = request.form['name']
+		password = request.form['password'] 
+		#--stopped 
 app.debug = True
 app.run()
