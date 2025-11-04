4232e5add1bec445f1326b0623aa6e8d97a276a8
Natalie <nataliekkeiger@gmail.com>
2025-11-04 13:49:48 -0500

    i think i broke it but idk what to do

--- app/__init__.py
+++ app/__init__.py
@@ -25,9 +25,12 @@ c.execute("INSERT INTO users VALUES('ricefarmer', 'riceplant'),('ricefarmer2', '
 db.commit()
 db.close()
 
-@app.route("/", methods=['GET', 'POST'])
-def login_page():
-    #session.pop('username',None) #to reset session since we dont have logout yet
+#trying to make proxy page that does the logic, i was running into an issue
+#with trying to make it only redirect you after you click the button
+#not sure now
+
+@app.route('/redirect_login', methods=['POST'])
+def redirect_login():
     if 'username' in session:
         return redirect('/home')
     testUser = request.form.get('username')
@@ -38,19 +41,47 @@ def login_page():
     check = c.execute(f"SELECT COUNT(*) FROM users WHERE username = '{testUser}' AND password = '{testPass}';")
     result = check.fetchone()[0]
     if result == 0:
-        return render_template('login.html')
+        db.commit()
+        db.close()
+        return redirect('/')
     else:
+        db.commit()
+        db.close()
         session['username'] = request.form.get('username')
         return redirect('/home')
-    db.commit()
-    db.close()
+
+
+@app.route("/", methods=['GET', 'POST'])
+def login_page():
+    #session.pop('username',None) #to reset session since we dont have logout yet
     return render_template('login.html')
+
 
 @app.route("/home")
 def home_page():
     if not session.get('username'):
-        return redirect('/login.html')
+        return redirect('/')
     return render_template('home.html')
+
+@app.route('/redirect_create', methods=['POST', 'GET'])
+def redirect_create():
+    if 'username' in session:
+        return redirect('/home')
+    db = sqlite3.connect(ACC_FILE)
+    c = db.cursor()
+    testUser = request.form.get('username')
+    testPass = request.form.get('password')
+    check = c.execute(f"SELECT COUNT(*) FROM users WHERE username = '{testUser}' AND password = '{testPass}';")
+    result = check.fetchone()[0]
+    if result == 0:
+        c.execute(f"INSERT INTO users VALUES('{testUser}', '{testPass}');")
+        db.commit()
+        db.close()
+        return redirect('/home')
+    else: 
+        db.commit()
+        db.close()
+        return redirect('/create_acc')
 
 @app.route("/create_acc")
 def register_page():
