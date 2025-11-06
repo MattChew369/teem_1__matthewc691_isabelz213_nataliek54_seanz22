57b14f016622149654f5052782683b0565051f04
szheng60stuy <szheng60@stuy.edu>
2025-11-05 20:56:46 -0500

    fixed login

--- app/__init__.py
+++ app/__init__.py
@@ -21,9 +21,9 @@ app.config['SESSION_TYPE'] = 'filesystem'
 app.secret_key = 'asdhajskjbweifnoihgis'
 
 #table for testing (remove after register works)
-#c.execute("DROP TABLE if EXISTS users;")
+c.execute("DROP TABLE if EXISTS users;")
 c.execute("CREATE TABLE IF NOT EXISTS users(username text primary key, password text);")
-#c.execute("INSERT INTO users VALUES('ricefarmer', 'riceplant'),('ricefarmer2', 'ricerice'),('ricefarmer3','ecir');")
+c.execute("INSERT INTO users VALUES('ricefarmer', 'riceplant'),('ricefarmer2', 'ricerice'),('ricefarmer3','ecir');")
 db.commit()
 db.close()
 #table for testing (remove after stories.db works)
@@ -37,36 +37,41 @@ sdb.close()
 #with trying to make it only redirect you after you click the button
 #not sure now
 
-@app.route('/redirect_login', methods=['POST', 'GET'])
+@app.route("/redirect_login", methods=['GET', 'POST'])
 def redirect_login():
+    #session.pop('username',None) #to reset session since we dont have logout yet
+    if 'username' in session:
+        return redirect('/home')
     testUser = request.form.get('username')
     testPass = request.form.get('password')
-    if not testUser or not testPass:
-        return redirect('/')
 
     db = sqlite3.connect(ACC_FILE)
     c = db.cursor()
-    check = c.execute(f"SELECT COUNT(*) FROM users WHERE username = {testUser} AND password = {testPass}")
+    check = c.execute(f"SELECT COUNT(*) FROM users WHERE username = '{testUser}' AND password = '{testPass}';")
     result = check.fetchone()[0]
     if result == 0:
-        db.close()
-        return redirect('/')
+        return render_template('login.html')
     else:
-        db.close()
-        session['username'] = testUser
+        session['username'] = request.form.get('username')
         return redirect('/home')
+    db.commit()
+    db.close()
+    return render_template('login.html')
 
 
 @app.route("/", methods=['GET', 'POST'])
 def login_page():
+    if 'username' in session:
+        return redirect('/home')
     #session.pop('username',None) #to reset session since we dont have logout yet
     return render_template('login.html')
 
 
 @app.route("/home", methods=['GET', 'POST'])
 def home_page():
-    #if not session.get('username'):
-    #    return redirect('/')
+    if not session.get('username'):
+        return redirect('/')
+    print("username: " + session['username'])
     return render_template('home.html')
 
 @app.route('/redirect_create', methods=['POST', 'GET'])
@@ -77,10 +82,10 @@ def redirect_create():
         return redirect('/create_acc')
     db = sqlite3.connect(ACC_FILE)
     c = db.cursor()
-    check = c.execute(f"SELECT COUNT(*) FROM users WHERE username = {testUser}")
+    check = c.execute(f"SELECT COUNT(*) FROM users WHERE username = {testUser};")
     result = check.fetchone()[0]
     if result == 0:
-        c.execute(f"INSERT INTO users VALUES ({testUser}, {testPass})")
+        c.execute(f"INSERT INTO users VALUES ({testUser}, {testPass});")
         db.commit()
         db.close()
         session['username'] = testUser
